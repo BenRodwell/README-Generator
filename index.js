e5c38237298bb95fc6c9a7fa41bb6bbf723ad74c
Ben Rodwell <benrodwell9@gmail.com>
2024-02-06 16:59:15 +1100

    Added the specific parameters

--- index.js
+++ index.js
@@ -4,23 +4,23 @@ const fs = require('fs');
 inquirer.prompt([
     {
         type: 'input',
+        name: 'username',
+        message:'What is your GitHub username?',
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is your email address?',
+    }, 
+    {
+        type: 'input',
         name: 'title',
-        message:'What is your README title?',
+        message: 'What is your projects name?',
     },
     {
         type: 'input',
         name: 'description',
-        message: 'What is your README description?',
-    }, 
-    {
-        type: 'input',
-        name: 'tableOfContents',
-        message: 'Do you have a table of contents for your README document?',
-    },
-    {
-        type: 'input',
-        name: 'installation',
-        message: 'What is your preferred style of installation?',
+        message: 'Please write a short description of your project',
     },
     {
         type: 'input',
@@ -30,60 +30,89 @@ inquirer.prompt([
     {
         type: 'checkbox',
         name: 'license',
-        message: 'What license does your web application have?',
-        choices: ['MIT', 'Chocolate'],
+        message: 'What kind of license should your project have?',
+        choices: ['MIT','APACHE 2.0', 'GPL 3.0', 'BSD 3', 'none'],
     },
     {
         type: 'input',
-        name: 'contributing',
-        message: 'Who contributed to your web application?',
+        name: 'dependencies',
+        message: 'What command should be run to install dependencies',
     },
     {
         type: 'input',
         name: 'tests',
-        message: 'What kinds of tests have you conducted on your web application?',
+        message: 'What command should be run to run tests?',
     },
     {
         type: 'input',
-        name: 'questions',
-        message: 'Are there any questions alluding to your web application?',
+        name: 'using',
+        message: 'What does the user need to know about using the repo?',
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'What does the user need to know about contributing to the repo',
     },
 ])
 .then((answers) => {
-    const html = generateHTML(answers);
-    console.log(html)
+    const README = generateREADME(answers);
+    console.log('Generating README file...')
 
 
-fs.appendFile('index.html', html, (err) =>
+fs.appendFile('README.md', README, (err) =>
 err ? console.error(err) : 'Success!'
 );
 
 });
 
-const generateHTML = (title, description,tableOfContents, installation, usage, license, contributing, tests, questions ) => {
-    return `<!DOCTYPE html>
-    <html lang="en">
-        <head>
-            <meta charset="UTF-8">
-            <meta http-equiv = "X-UA-Compatible" content = "ie=edge">
-            <link rel = "stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css">
-            <title>Title</title>
-        </head>
-        <body>
-            <header class = "p-5 mb-4 header bg-light">
-                <div class = "container">
-                    <h1 class = "display-4"> Title: ${title}</h1>
-                    <h3 class = "display-4"> Description: ${description}</h1>
-                    <h1 class = "display-4"> Table of Contents: ${tableOfContents}</h1>
-                    <h1 class = "display-4"> Installation: ${installation}</h1>
-                    <h1 class = "display-4"> Usage: ${usage}</h1>
-                    <h1 class = "display-4"> License: ${license}</h1>
-                    <h1 class = "display-4"> Contributing: ${contributing}</h1>
-                    <h1 class = "display-4"> Tests: ${tests}</h1>
-                    <h1 class = "display-4"> Questions: ${questions}</h1>
-                </div>
-            </header>
-        </body>
-    </html> `
+const generateREADME = ({title, description, tableOfContents, username, email, installation, usage, license, dependencies, tests, using, contributing}) => {
+return `# ${title}
+
+## Description
+    
+${description}
+    
+## Table of Contents
+    
+${tableOfContents}
+
+## Installation
+To install necessary dependencies, run the following command:
+${installation}
+    
+## Usage
+    
+${usage}
+    
+## Credits
+    
+N/A
+    
+## License
+    
+This project is licensed under the ${license} license.
+    
+## Dependencies
+    
+${dependencies}
+    
+## Tests
+    
+${tests}
+
+## Using
+    
+${using}
+    
+## How to Contribute
+    
+${contributing}
+
+## Questions
+If you have any questions about the repo, open an aissue or contact me directly at ${email}. You can find more of my work at ${username}.
+   
+      `
 }
+
+
 
